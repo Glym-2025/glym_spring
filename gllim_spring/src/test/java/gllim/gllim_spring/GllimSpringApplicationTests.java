a969c4896a1562d96ef89ecf7027954baccf511e
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2025-03-14 22:17:55 +0900

    init

---
+++ gllim_spring/src/test/java/gllim/gllim_spring/GllimSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gllim.gllim_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GllimSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
